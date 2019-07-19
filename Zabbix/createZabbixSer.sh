0f21940a461d32833afe85938289a9499430db4f
sanwenjing <sanwenjing@163.com>
2019-07-19 10:51:18 +0800

    create SNMP dir

---
+++ Zabbix/createZabbixSer.sh
@@ -0,0 +1,32 @@
+#start a zabbix server by docker
+#step 1
+docker run --restart=always --name mysql-server -t \
+      -e MYSQL_DATABASE="zabbix" \
+      -e MYSQL_USER="zabbix" \
+      -e MYSQL_PASSWORD="zabbix" \
+      -e MYSQL_ROOT_PASSWORD="zabbix" \
+      -d mysql:5.7  \
+--character-set-server=utf8 --collation-server=utf8_bin 
+
+#step 2
+docker run --restart=always --name zabbix-server-mysql -t \
+      -e DB_SERVER_HOST="mysql-server" \
+      -e MYSQL_DATABASE="zabbix" \
+      -e MYSQL_USER="zabbix" \
+      -e MYSQL_PASSWORD="zabbix" \
+      -e MYSQL_ROOT_PASSWORD="zabbix" \
+      --link mysql-server:mysql \
+      -p 10051:10051 \
+      -d zabbix/zabbix-server-mysql:latest
+
+#step 3
+docker run --restart=always --name zabbix-web-nginx-mysql -t \
+      -e DB_SERVER_HOST="mysql-server" \
+      -e MYSQL_DATABASE="zabbix" \
+      -e MYSQL_USER="zabbix" \
+      -e MYSQL_PASSWORD="zabbix" \
+      -e MYSQL_ROOT_PASSWORD="zabbix" \
+      --link mysql-server:mysql \
+      --link zabbix-server-mysql:zabbix-server \
+      -p 80:80 \
+      -d zabbix/zabbix-web-nginx-mysql:latest

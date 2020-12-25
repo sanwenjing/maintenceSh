12ef8cc86346bc930df0230e89869e5da09b3c38
sanwenjing <sanwenjing@163.com>
2020-12-25 22:09:43 +0800

    alter the port

--- Docker/registry/createRegistry.sh
+++ Docker/registry/createRegistry.sh
@@ -1,6 +1,6 @@
 #building a registry server
 docker run --restart=always -d \
     -v /opt/registry:/var/lib/registry \
-    -p 5000:5000 \
+    -p 80:5000 \
     --name myregistry \
     registry:2 

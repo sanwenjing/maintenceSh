f98a362b05720486d6ff4ebb5738267ae17cdbef
sanwenjing <sanwenjing@163.com>
2020-12-26 11:19:38 +0800

    alter the commit

--- Docker/osTicket/createOsTicket2.sh
+++ Docker/osTicket/createOsTicket2.sh
@@ -6,7 +6,7 @@ docker run --restart=always -d \
     -e MYSQL_PASSWORD=123456 \
     -e MYSQL_DATABASE=osticket \
     --name osticket_mysql\
-    192.168.31.151:5000/mysql:latest
+    repo.local.com/mysql:5.7.26
 #building a osTicket server
 #docker run --restart=always -d --name osticket --link osticket_mysql:mysql -p 80:80 devinsolutions/osticket
 docker run --restart=always -d \
@@ -17,7 +17,7 @@ docker run --restart=always -d \
     --link osticket_mysql:mysql \
      --name osticket\
      -p 80:80 \
-     192.168.31.151:5000/osticket:20201224
+     repo.local.com/osticket:20201224
 echo "http://localhost/scp. Login with default admin user & password:"
 echo "username: ostadmin"
 echo "password: Admin1"

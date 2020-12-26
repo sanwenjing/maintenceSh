63074d2b4e9ff63d3583d732abb81b93a8b531e3
sanwenjing <sanwenjing@163.com>
2020-12-26 20:50:37 +0800

    nextcloud add

---
+++ Docker/nextcloud/createNextcloud.sh
@@ -0,0 +1,12 @@
+echo "Create a server for Nextcloud?(y/n)"
+read key
+if [ $key = 'y' ]; then
+#mkdir -p /home/ncdata
+#chmod -R 777 /home/ncdata
+docker run -d --restart=always \
+-v nextcloud:/var/www/html \
+--name nextcloud \
+-p 80:80 nextcloud
+fi
+echo "http://localhost/ Login and install after few minutes"
+

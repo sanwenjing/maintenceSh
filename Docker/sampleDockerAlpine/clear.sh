f4dea7761a0139c549136713c38b3cdea9498d44
sanwenjing <sanwenjing@163.com>
2022-01-15 22:07:48 +0800

    add sampleDockerAlpine

---
+++ Docker/sampleDockerAlpine/clear.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+target=alpine-base
+ver=1.0
+images=repo.local.com/$target:$ver
+
+echo "clear $target and delete $images(y/n)"
+read key
+if [ $key = 'y' ]; then
+docker stop $target
+docker rm $target
+docker rmi $images
+fi
+
+
+echo "Rebuild for $images(y/n)"
+read key
+if [ $key = 'y' ]; then
+docker build -t $images .
+docker images
+echo "Mission completed!"
+fi
+
+
+echo "Building a server for $sn?(y/n)"
+read key
+if [ $key = 'y' ]; then
+docker run --restart=always -d -it \
+    --name $target \
+repo.local.com/$target:$ver
+#sanwenjing/np:1.0
+fi
+echo "Visit web http://localhost/ via webrower"
+

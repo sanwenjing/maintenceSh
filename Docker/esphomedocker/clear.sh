daacd15deccb7c3d58ef092a94769fe81a623245
sanwenjing <sanwenjing@163.com>
2022-03-06 09:41:20 +0800

    esphome add

---
+++ Docker/esphomedocker/clear.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+target=esphome
+ver=1.0
+images=repo.local.com/$target:$ver
+#images=esphome/esphome:latest
+
+echo "clear $target and delete $images(y/n)"
+read key
+if [ $key = 'y' ]; then
+docker stop $target
+docker rm $target
+docker rmi $images
+fi
+
+
+echo "Rebuild for $images(y/n)"
+read key
+if [ $key = 'y' ]; then
+docker build -t $images .
+docker images
+echo "Mission completed!"
+fi
+
+
+echo "Building a server for $sn?(y/n)"
+read key
+if [ $key = 'y' ]; then
+docker run --restart=always -d -it \
+-p 6052:6052 \
+    --name $target \
+repo.local.com/$target:$ver
+#sanwenjing/np:1.0
+fi
+echo "Visit web http://localhost:6052/ via webrower"
+

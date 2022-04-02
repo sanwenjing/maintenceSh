503f28291d0e4e4bbebbee35e4670662c57acfd4
sanwenjing <sanwenjing@163.com>
2022-04-02 09:46:34 +0800

    add frps

---
+++ Docker/frpDocker/server/clear.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+target=alpine-frp-server
+ver=0.37
+images=repo.local.com/$target:$ver
+
+echo "clear $target and delete $images(y/n)"
+read key
+if [ $key = 'y' ]; then
+docker stop $target
+docker rm $target
+docker rmi $images
+fi
+
+
+echo "Rebuild for $images(y/n)"
+read key
+if [ $key = 'y' ]; then
+docker build -t $images .
+docker images
+echo "Mission completed!"
+fi
+
+
+echo "Building a server for $sn?(y/n)"
+read key
+if [ $key = 'y' ]; then
+docker run --restart=always -d \
+    --name $target \
+--net=host \
+$images
+#sanwenjing/np:1.0
+docker logs $target
+fi
+#echo "Visit web http://localhost/ via webrower"
+echo "connect via client"
+

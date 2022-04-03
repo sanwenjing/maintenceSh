a6653bab2c992a2f48b9b13fe1f5dcdc2e708360
sanwenjing <sanwenjing@163.com>
2022-04-03 08:55:23 +0800

    update for base clear.sh

--- Docker/baseFile/clear.sh
+++ Docker/baseFile/clear.sh
@@ -24,10 +24,10 @@ fi
 echo "Building a server for $sn?(y/n)"
 read key
 if [ $key = 'y' ]; then
-docker run --restart=always -d \
-    --name $target \
+docker run --restart=always -d --name $target \
     -p 80:80 \
-repo.local.com/$target:$ver    
+$images
+#repo.local.com/$target:$ver    
 #sanwenjing/np:1.0
 fi
 echo "Visit web http://localhost/ via webrower"

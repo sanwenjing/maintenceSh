7f7dbb833765bd2f65e1239107f0aa3e8f46ddef
sanwenjing <sanwenjing@163.com>
2022-07-31 13:48:11 +0800

    update openRoot.sh for reset root password

--- System/mysql/openRoot.sh
+++ System/mysql/openRoot.sh
@@ -5,3 +5,10 @@ GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION
 flush privileges;
 fi
 
+echo "Do you want to reset root password?(y/n)"
+read key
+if [ $key = 'y' ]; then
+#GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;
+set password for 'root'@'%' = password('123456');
+flush privileges;
+fi

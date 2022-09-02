4721cd8ca7c8e9364d9469609d840a0bb6b8b6df
sanwenjing <sanwenjing@163.com>
2022-09-02 20:33:05 +0800

    update OffSELinux.sh

--- System/OffSELinux.sh
+++ System/OffSELinux.sh
@@ -2,6 +2,6 @@ echo "Are your want to turn off the SELinux?(y/n)"
 read key
 if [ $key = 'y' ]; then
 setenforce 0
-sed -i "s/enforcing/disabled/g" /etc/selinux/config
+sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
 fi
 

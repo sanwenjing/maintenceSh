16682b6ab2f44775ef39c5bd4c24651e8116b2b2
sanwenjing <sanwenjing@163.com>
2022-09-03 11:23:32 +0800

    update ost

--- Docker/ostdocker/clear.sh
+++ Docker/ostdocker/clear.sh
@@ -1,15 +1,38 @@
 #!/bin/sh
 target=ost
-images=repo.local.com/ost:1.15.1
+ver=1.15.1
+images=repo.local.com/$target:$ver
+#repo.local.com/ost:1.15.1
 
+echo "clear $target and delete $images(y/n)"
+read key
+if [ $key = 'y' ]; then
 docker stop $target
 docker rm $target
 docker rmi $images
+fi
+
+
 echo "Rebuild for $images(y/n)"
 read key
 if [ $key = 'y' ]; then
 docker build -t $images .
 docker images
-
-
+echo "Mission completed!"
 fi
+
+
+echo "Building a server for $sn?(y/n)"
+read key
+if [ $key = 'y' ]; then
+
+docker run --restart=always -d \
+    --name $target \
+    -p 80:80 \
+    -v /etc/localtime:/etc/localtime:ro \
+$images
+docker logs $target
+fi
+echo "Visit web http://localhost/ via webrower"
+
+
